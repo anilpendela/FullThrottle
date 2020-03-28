77e9464ea11322e65ba1f4dc29380631962e40f4
anilpendela <anilpendela1106@gmail.com>
2020-03-28 17:15:23 +0530

    Initial Commit 2

---
+++ FullTrottle/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class ActivityPeriods(models.Model):
+    start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+class User(AbstractBaseUser):
+    user_id = models.CharField(max_length=16, null=True, blank=True)
+    tz = models.CharField(max_length=64, null=True, blank=True)
+    activity_periods = models.ManyToManyField(to=ActivityPeriods)
